9f4dc04c0bf0543c732f1769d48aa2ec544213c6
Guillermo Villalta <mexcelus@gmail.com>
2019-09-29 23:38:53 -0600

    word can be auto generated or created + using jsfire to handle CLI

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const figlet = require('figlet');
 const colors = require('colors');
+const fire = require('js-fire');
+const inquirer = require('inquirer')
 
 colors.setTheme({
     silly: 'rainbow',
@@ -13,18 +15,57 @@ colors.setTheme({
     debug: 'blue',
     error: 'red'
 });
-
-figlet.text('Guillermo', {
-    font: '3-d',
-    horizontalLayout: 'default',
-    verticalLayout: 'default'
-}, function (err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(colors.silly(data));
-    console.log(data.length);
     
-});
+var makeWord = {
+    auto: () => { 
+        figlet.text('word', {
+            font: '3-d',
+            horizontalLayout: 'default',
+            verticalLayout: 'default'
+        }, function (err, data) {
+            if (err) {
+                console.log('Something went wrong...');
+                console.dir(err);
+                return;
+            }
+        
+            // console.log(data.length);
+            console.log(colors.silly(data));
+        });
+    },
+    create: () => {
+        inquirer
+            .prompt([
+                /* Pass your questions in here */
+                {
+                    type: "input",
+                    message: "Text to ASCII:",
+                    name: "text"
+                },
+                {
+                    type: "input",
+                    message: "Font?",
+                    name: "font"
+                }
+            ])
+            .then(answers => {
+                // Use user feedback for... whatever!!
+                figlet.text(answers.text, {
+                    font: answers.font,
+                    horizontalLayout: 'default',
+                    verticalLayout: 'default'
+                }, function (err, data) {
+                    if (err) {
+                        console.log('Something went wrong...');
+                        console.dir(err);
+                        return;
+                    }
+        
+                    // console.log(data.length);
+                    console.log(colors.verbose(data));
+                });
+            });
+    }
+}
+
+fire(makeWord);
