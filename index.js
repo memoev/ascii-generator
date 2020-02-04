cb97e19f8d74d69d7f69bb6a768cc4c850824e8f
Guillermo Villalta <mexcelus@gmail.com>
2020-02-03 21:16:40 -0700

    color

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ var makeWord = {
             }
         
             // console.log(data.length);
-            console.log(colors.error(data));
+            console.log(colors.warn(data));
         });
     },
     // Word being created with: node index auto on console
